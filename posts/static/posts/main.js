68856e0e4256847f8ed431db9148cf704f6df761
Tien Young <constantine.sky@hotmail.com>
2025-04-10 22:11:36 -0400

    Getting Posts Objects with Ajax

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,1 +1,33 @@
 console.log('hello world')
+
+const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response)
+        helloWorldBox.innerHTML = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log('success', response)
+        const data = response.data
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `
+        });
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
